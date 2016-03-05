e32e31fb33f1762a7527f91c08aa006f0974d042
Vasileios Samaltanos <vas@ozschooltek.com.au>
2016-03-05 23:20:07 +1100

    Added location services

---
+++ src/Thalia/Services/Location/GeoLiteService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using Thalia.Data;
+using Thalia.Extensions;
+using System.Linq;
+using Thalia.Data.Entities;
+
+namespace Thalia.Services.Location
+{
+    public class GeoLiteService : ILocationApi
+    {
+        #region private members
+        private ILogger _logger;
+        private ThaliaContext _context;
+        #endregion
+
+        public GeoLiteService(ILogger logger, ThaliaContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public bool CanRequest()
+        {
+            return true;
+        }
+
+        public Location GetLocation(string json)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Thalia/Services/Location/IpGeolocationApi.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using Thalia.Data;
+using Thalia.Extensions;
+using System.Linq;
+using Thalia.Data.Entities;
+
+namespace Thalia.Services.Location
+{
+    public class IpGeolocationApi : ILocationApi
+    {
+        #region private members
+        private ILogger _logger;
+        private ThaliaContext _context;
+        private int RequestsPerMinute = 150;
+        #endregion
+
+        public IpGeolocationApi(ILogger logger, ThaliaContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public bool CanRequest()
+        {
+            return _context.ServiceRequests.Where(x => x.Api == this.GetType().Name && x.Created >= DateTime.Now.AddHours(-1) && x.Created <= DateTime.Now).Count() <= RequestsPerMinute;
+        }
+
+        public Location GetLocation(string json)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
