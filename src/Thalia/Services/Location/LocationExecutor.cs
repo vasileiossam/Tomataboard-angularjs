3827e2f8f610380bc394f396c582f6b9dc1d7ceb
Vasileios Samaltanos <vas@ozschooltek.com.au>
2016-03-08 22:44:02 +1100

    Added LocationExecutor

---
+++ src/Thalia/Services/Location/LocationExecutor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+using Thalia.Data;
+using Thalia.Services.Cache;
+
+namespace Thalia.Services.Location
+{
+    public class LocationExecutor : ServiceExecutor<Location>, IServiceExecutor<Location>
+    {
+        public LocationExecutor(ILogger logger, ICacheRepository<Location> cacheRepository, ThaliaContext context)
+            : base(logger, cacheRepository)
+        {
+            // todo change this with a container that will register the types instead
+            _operations.Add(new IpGeolocationApi(logger));
+            _operations.Add(new GeoLiteService(logger, context));
+            _operations.Add(new FreegeoipService(logger));
+        }
+    }
+}
