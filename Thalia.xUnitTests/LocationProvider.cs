ea54873fdfd8c3760a5a2af1a667380d52f2ef06
Vasileios Samaltanos <vas@ozschooltek.com.au>
2016-03-12 16:42:41 +1100

    Added xUnitTests

---
+++ Thalia.xUnitTests/LocationProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Thalia.Services.Photos;
+using Xunit;
+
+namespace Thalia.xUnitTests
+{
+    /// <summary>
+    /// https://xunit.github.io/docs/getting-started-dnx.html
+    /// </summary>
+    public class LocationProvider
+    {
+        public LocationProvider()
+        {
+        }
+
+        
+        [Fact]
+        public void PassingTest()
+        {
+            var provider = new PhotoProvider();
+        }
+    }
+}
