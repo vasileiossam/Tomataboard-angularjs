72f7e2ad1c2034bb264d167e74d086a8896665b1
Vasileios Samaltanos <vas@ozschooltek.com.au>
2016-04-17 18:52:24 +1000

    Converted sidebar to custom directive

--- src/Tomataboard/wwwroot/js/dashboardController.js
+++ src/Tomataboard/wwwroot/js/dashboardController.js
@@ -1,7 +1,3 @@
-$(document).ready(function () {
-    $('nav').slideAndSwipe();
-});
-
 (function () {
     "use strict";
 
--- src/Tomataboard/wwwroot/js/sidebar.js
+++ src/Tomataboard/wwwroot/js/sidebar.js
@@ -1,1 +1,23 @@
-﻿
+﻿(function () {
+    "use strict";
+
+    var app = angular.module("dashboard-app");
+    
+    app.directive("sidebar", function () {
+
+        return {
+            restrict: "E",
+            replace: "true",
+
+            templateUrl: '/views/sidebar.html',
+
+            link: function (scope, element, attrs) {
+                $(document).ready(function () {
+                    $('nav').slideAndSwipe();
+                });
+
+            }
+        };
+    });
+
+})();
