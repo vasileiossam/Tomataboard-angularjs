81645eed343f356bbb5185089da9be78af3ae255
Vasileios Samaltanos <vas@ozschooltek.com.au>
2016-04-19 19:32:29 +1000

    clock: toggle format on click (cursor)

--- src/Tomataboard/wwwroot/js/clock.js
+++ src/Tomataboard/wwwroot/js/clock.js
@@ -17,16 +17,27 @@
             },
 
             link: function (scope, element, attrs) {
-                var timeoutId;
+
+                // toggle time format
+                element.bind("click", function () {
+                    if (scope.format === "12-hour") {
+                        scope.format = "24-hour";
+                    }
+                    else
+                        if (scope.format === "24-hour") {
+                            scope.format = "12-hour";
+                        }
+                    scope.$apply();
+                });
 
                 var tick = function () {
                     scope.time = Date.now();
                 }
                 tick();
 
-                timeoutId = $interval(tick, 1 * 60 * 1000);
+                var timeoutId = $interval(tick, 1 * 60 * 1000);
 
-                element.on('$destroy', function () {
+                element.on("$destroy", function () {
                     $interval.cancel(timeoutId);
                 });
             }
