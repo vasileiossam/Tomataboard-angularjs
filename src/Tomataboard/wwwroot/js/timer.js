db886532b54a26c333092de85afaed6ad1081874
Vasileios Samaltanos <vas@ozschooltek.com.au>
2016-04-21 21:06:55 +1000

    Clone stopwatch to timer

--- src/Tomataboard/wwwroot/js/timer.js
+++ src/Tomataboard/wwwroot/js/timer.js
@@ -3,7 +3,7 @@
 
     var app = angular.module("dashboard-app");
 
-    app.directive("timer", function () {
+    app.directive("timer", function ($interval) {
 
         return {
             restrict: "E",
@@ -11,8 +11,44 @@
 
             templateUrl: '/views/timer.html',
 
-            link: function(scope, element, attrs) {
+            link: function (scope, element, attrs) {
+                var timeoutId;
+                var seconds = 0;
 
+                var tick = function () {
+                    seconds = seconds + 1;
+                    var date = new Date(null);
+                    date.setSeconds(seconds);
+                    scope.time = date.toISOString().substr(11, 8);
+                }
+
+                scope.start = function () {
+                    if (timeoutId) {
+                        scope.reset();
+                    } else {
+                        scope.startText = "STOP";
+                        timeoutId = $interval(tick, 1 * 1000);
+                    }
+                };
+
+                scope.reset = function () {
+                    scope.time = "00:00:00";
+                    scope.startText = "START";
+                    if (timeoutId) {
+                        $interval.cancel(timeoutId);
+                        timeoutId = 0;
+                        seconds = 0;
+                    }
+                };
+
+                scope.reset();
+                var state = 0;
+
+
+
+                element.on("$destroy", function () {
+                    $interval.cancel(timeoutId);
+                });
             }
         };
     });
