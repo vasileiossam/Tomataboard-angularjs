71bd0f3f7821cfd7d6ab1b1cae961d2231777e4b
Vasileios Samaltanos <vas@ozschooltek.com.au>
2016-05-08 15:59:52 +1000

    Install and use angular-bootstrap-checkbox

--- src/Tomataboard/wwwroot/js/dashboard-app.js
+++ src/Tomataboard/wwwroot/js/dashboard-app.js
@@ -1,7 +1,13 @@
 (function() {
 
-    angular.module("dashboard-app", ['ngCookies', 'ngStorage', 'ngAnimate',
-        'ngAudio', 'angularScreenfull', 'ui.bootstrap.datetimepicker',
-        'as.sortable']);
+    angular.module("dashboard-app", [
+        "ngCookies",
+        "ngStorage",
+        "ngAnimate",
+        "ngAudio",
+        "angularScreenfull",
+        "ui.bootstrap.datetimepicker",
+        "as.sortable",
+        "ui.checkbox"]);
 
 })();
