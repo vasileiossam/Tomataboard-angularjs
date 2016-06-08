5458c83226e67f789639c8f3de7b05a3ff26dde9
Vasileios Samaltanos <vas@ozschooltek.com.au>
2016-06-08 21:01:00 +1000

    Gulp tasks to bump version number and update settings.html

--- src/Tomataboard/gulpfile.js
+++ src/Tomataboard/gulpfile.js
@@ -6,7 +6,20 @@ var gulp = require("gulp"),
     concat = require("gulp-concat"),
     cssmin = require("gulp-cssmin"),
     uglify = require("gulp-uglify"),
+    fs = require('fs'),
+
+    // add angularjs dependency injection annotations 
     ngAnnotate = require("gulp-ng-annotate");
+
+// version bumping
+var bump = require("gulp-bump");
+
+// replace build blocks in HTML
+var htmlreplace = require("gulp-html-replace");
+
+// run a series of dependent gulp tasks in order
+// This is intended to be a temporary solution until the release of gulp 4.0 
+var runSequence = require('run-sequence');
 
 var del = require("del");
 
@@ -23,6 +36,8 @@ paths.minCss = paths.webroot + "css/**/*.min.css";
 paths.concatJsDest = paths.webroot + "js/dashboard.min.js";
 paths.concatCssDest = paths.webroot + "css/dashboard.min.css";
 
+// ------ clean -------
+
 gulp.task("clean:js", function (cb) {
     rimraf(paths.concatJsDest, cb);
 });
@@ -31,7 +46,14 @@ gulp.task("clean:css", function (cb) {
     rimraf(paths.concatCssDest, cb);
 });
 
-gulp.task("clean", ["clean:js", "clean:css", "clean:logfiles"]);
+// https://github.com/gulpjs/gulp/blob/master/docs/recipes/delete-files-folder.md
+gulp.task("clean:logfiles", function () {
+    return del([
+       paths.webroot + "*.log"
+    ]);
+});
+
+// ------ minification  -------
 
 // minimize dashboard
 gulp.task("min:js", function () {
@@ -61,11 +83,38 @@ gulp.task("min:css", function () {
         .pipe(gulp.dest("."));
 });
 
-gulp.task("min", ["min:js", "min:css"]);
+var getProjectJson = function () {
+    return JSON.parse(fs.readFileSync('./project.json', 'utf8'));
+};
 
-// https://github.com/gulpjs/gulp/blob/master/docs/recipes/delete-files-folder.md
-gulp.task("clean:logfiles", function() {
-     return del([
-        paths.webroot  + "*.log"
-    ]); 
+// bump version number
+gulp.task("bump", function () {
+    return gulp.src("./project.json")
+    .pipe(bump())
+    .pipe(gulp.dest("./"));
 });
+
+// set version number in html
+gulp.task("version:html", ['bump'], function (callback) {
+    gulp.src("./wwwroot/app/settings/settings.html")
+     .pipe(htmlreplace({
+         'version': getProjectJson().version
+     },
+     {
+         keepBlockTags: true
+     }))
+    .pipe(gulp.dest("./wwwroot/app/settings/"));
+
+    callback();
+});
+
+// ----------------------------
+// prepublish tasks
+// ----------------------------
+gulp.task("version", function(callback) {
+    runSequence('bump',
+              'version:html',
+              callback);
+              });
+gulp.task("clean", ["clean:js", "clean:css", "clean:logfiles"]);
+gulp.task("min", ["min:js", "min:css"]);
